77759aaceb88d2ee8b98c95276f3f5a94ba9d9ff
Daniyel Cheah <cheahd@mcmaster.ca>
2022-05-23 17:38:59 -0400

    added colors to text

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import { franc } from 'franc'
-import langs from 'langs'
+import { franc } from 'franc';
+import langs from 'langs';
+import colors from 'colors';
+
 const input = process.argv[2];
 const langCode = franc(input)
 
 if (langCode == 'und') {
-    console.log("Input not recognized, please try again");
+    console.log("Input not recognized, please try again".red);
 } else {
     const language = langs.where("3", langCode);
-    console.log(`This language is most likely: ${language.name}`);
+    console.log(`This language is most likely: ${language.name}`.cyan);
 }
