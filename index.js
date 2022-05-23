f2bae32f08180058d330e5eaee023608b2e697b7
Daniyel Cheah <cheahd@mcmaster.ca>
2022-05-23 17:28:43 -0400

    finalized

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { franc } from 'franc';
-const langs = import('langs');
+import {franc} from 'franc'
+import langs from 'langs'
 
 const input = process.argv[2];
-const code = franc(input);
-const language = langs.where("3", code);
+const langCode = franc(input)
 
+const language = langs.where("3", langCode);
 console.log(language.name);
