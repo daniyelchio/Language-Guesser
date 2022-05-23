ccda28f33220d8f068354d89dacf9186460b9aea
Daniyel Cheah <cheahd@mcmaster.ca>
2022-05-23 17:32:41 -0400

    added try-catch for und

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import {franc} from 'franc'
+import { franc } from 'franc'
 import langs from 'langs'
-
 const input = process.argv[2];
 const langCode = franc(input)
 
-const language = langs.where("3", langCode);
-console.log(language.name);
+if (langCode == 'und') {
+    console.log("Input not recognized, please try again");
+} else {
+    const language = langs.where("3", langCode);
+    console.log(language.name);
+}
